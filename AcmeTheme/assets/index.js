3882178fc3b0ef371c68354c0a897832ff8acd67
Atishay Jain <contact@atishay.me>
2020-11-21 22:54:56 -0800

    Ch10-2

--- AcmeTheme/assets/index.js
+++ AcmeTheme/assets/index.js
@@ -1,26 +1,26 @@
 function addFormHandler() {
-  document.querySelectorAll('form')
+  document.querySelectorAll("[data-dynamic-form]")
     .forEach(async form => form.addEventListener('submit', async event => {
       event.preventDefault();
       const form = event.target;
-      let data = new FormData(form);
+      const data = new FormData(form);
       const action = form.action;
       const method = form.method;
       const response = await fetch(action, {
         method,
         body: data,
-        headers: { 'Accept': 'application/json'}
+        headers: {"Accept": "application/json"}
       });
       if (response.ok) {
           form.insertAdjacentHTML('afterend',
-            '<div class="success">The form has been submitted.</div>');
+            document.querySelector(form.dataset.success).innerHTML);
       } else {
         form.insertAdjacentHTML('afterend',
-          '<div class="error">The form could not be submitted. Please try again later.</div>');
+          document.querySelector(form.dataset.error).innerHTML);
       }
       const message = form.nextElementSibling;
       form.reset();
-      setTimeout(() => message.remove(), 2000);
+      setTimeout(() => message.remove(), 10000);
   }));
 }
 
